5bf28bd71e041d40f290ff3e26024422a1119be8
Kimberley Greenbush <v-kgreenbush@microsoft.com>
2021-09-15 23:38:49 -0700

    add MonthWithDays class

---
+++ Iterator/MonthWithDays.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    class MonthWithDays
+    {
+        public string Date { get; set; }
+        public int Days { get; set; }
+    }
+}
